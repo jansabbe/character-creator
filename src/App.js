e3a4c4acc7ecbd211f77d2c40d9417c02a03c578
Jan Sabbe <648689+jansabbe@users.noreply.github.com>
2022-06-29 22:42:12 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+import PerspectiveBar from "./components/PerspectiveBar";
 
 function App() {
   return (
     <>
       <CharacterEditor />
+      <PerspectiveBar />
       <Footer />
     </>
   );
---
+++ src/components/PerspectiveBar/index.js
@@ -0,0 +1,5 @@
+import styles from "./PerspectiveBar.module.css";
+
+export default function PerspectiveBar() {
+  return <div className={styles.perspective_bar}></div>;
+}
